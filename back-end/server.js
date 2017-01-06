ea3199fe60ba1eb68868a26cd471c41ec0d73caf
Pius Wanjiru <piusmuriithi1@gmail.com>
2017-01-06 17:04:06 +0300

    display all the comments from mongodb into the view

--- back-end/server.js
+++ back-end/server.js
@@ -15,6 +15,8 @@ app.use(function(req,res,next) {
 	next();
 })
 
+app.get('/api/message', GetComments);
+
 app.post('/api/message', function(req, res){
 	console.log(req.body);
 	var comment = new Comment(req.body);
@@ -22,16 +24,15 @@ app.post('/api/message', function(req, res){
 	res.status(200);
 })
 
-function GetComments() {
+function GetComments(req, res) {
 	Comment.find({}).exec(function (err, result) {
-		console.log(result);
+		res.send(result);
 	});
 }
 
 mongoose.connect("mongodb://localhost:27017/test", function (err, db) {
 	if (!err) {
 		console.log("we are connected to mongo");
-		GetComments();
 	}
 })
 
--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -3,10 +3,18 @@ export class MainController {
     'ngInject';
 
     this.$http = $http;
+    this.getComments();
 
   }
 
-  postMessage(){
+  getComments(){
+  	var vm = this;
+  	this.$http.get('http://localhost:5000/api/message').then(function (result) {
+  		vm.messages = result.data;
+  	});
+  }
+
+  postComment(){
     this.$http.post('http://localhost:5000/api/message', {msg: this.message});
   }
 
